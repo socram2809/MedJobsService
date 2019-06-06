5cd57c5fa04d6810886bb6ced0d521259642415a
Marcos Aurélio Macedo da Rocha Júnior <marcos@marcos>
2019-06-06 18:21:46 -0300

    Implementação de busca de Oportunidades

--- Oportunidade.js
+++ Oportunidade.js
@@ -4,10 +4,32 @@ var firebase = require('./Firebase.js')
 const database = firebase.database()
 const usuariosRef = database.ref('/oportunidade')
 
-//Retorna as oportunidades
+//Retorna todas as oportunidades
 router.get('/', function(req, res){
     usuariosRef.on('value', function(snapshot){
         res.send(JSON.stringify(snapshot.val()))  
+    })
+})
+
+//Retorna oportunidades específicas
+router.get('/busca/:busca', function(req, res){
+    var busca = req.params.busca.toUpperCase();
+    usuariosRef.on('value', function(snapshot){
+        var resultados = snapshot.val();
+        var valores = [];
+        var filtrado = [];
+        var keys = Object.keys(resultados)
+        keys.forEach((key) => {
+            valores.push(resultados[key]);
+        })
+        filtrado = valores.filter((oportunidade) => {
+            return oportunidade.cidade.toUpperCase().includes(busca) 
+                    || oportunidade.titulo.toUpperCase().includes(busca)
+                    || oportunidade.estado.toUpperCase().includes(busca) 
+                    || oportunidade.unidade.toUpperCase().includes(busca)
+                    || oportunidade.descricao.toUpperCase().includes(busca);
+        })
+        res.send(JSON.stringify(filtrado))  
     })
 })
 
