18a469df119bc046fa9fafbe07f0fe5b71437aa8
Marcos Aurélio Macedo da Rocha Júnior <marcos@marcos>
2019-06-17 22:04:00 -0300

    Método PUT agora é liberado

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8080
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS, PUT');
     next();
 });
 app.use(bodyParser.json())
